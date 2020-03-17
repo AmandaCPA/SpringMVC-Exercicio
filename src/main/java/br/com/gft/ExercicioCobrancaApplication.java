9b13cfadc0e1860ead298ac52525d8887b6a3f10
Amanda <amandacarvalhopintodeazevedo@gmail.com>
2020-03-17 15:27:13 -0300

    preparando ambiente Spring MVC

---
+++ src/main/java/br/com/gft/ExercicioCobrancaApplication.java
@@ -0,0 +1,13 @@
+package br.com.gft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicioCobrancaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercicioCobrancaApplication.class, args);
+	}
+
+}
