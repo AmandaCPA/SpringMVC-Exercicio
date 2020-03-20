be81afb4ab79899cb21a42b6ae6dedc23b2d034a
Amanda <amandacarvalhopintodeazevedo@gmail.com>
2020-03-20 15:06:05 -0300

    validações e redirecionamento de novo cadastro

--- src/main/java/br/com/gft/controller/TituloController.java
+++ src/main/java/br/com/gft/controller/TituloController.java
@@ -6,11 +6,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.gft.model.StatusTitulo;
 import br.com.gft.model.Titulo;
@@ -28,24 +30,34 @@ public class TituloController
 	public ModelAndView novo()
 	{
 		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		mv.addObject(new Titulo());
 		//mv.addObject("todosStatusTitulo", StatusTitulo.values());
 		return mv;
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
-	public ModelAndView salvar(Titulo titulo) 
+	public String salvar(@Validated Titulo titulo, Errors errors, RedirectAttributes attributes) 
 	{
+		if(errors.hasErrors())
+		{
+			return "CadastroTitulo";
+		}
+		
 		titulos.save(titulo);
-		ModelAndView mv = new ModelAndView("CadastroTitulo");
-		mv.addObject("mensagem", "Título salvo com sucesso!");
+		attributes.addFlashAttribute("mensagem", "Título salvo com sucesso");
+		return "redirect:/titulos/novo";
+		
 		//mv.addObject("todosStatusTitulo", StatusTitulo.values());
-		return mv;
+		
 	}
 	
 	@RequestMapping
-	public String pesquisar()
+	public ModelAndView pesquisar()
 	{
-		return "PesquisaTitulos";
+		List<Titulo> todosTitulos = titulos.findAll();
+		ModelAndView mv = new ModelAndView("PesquisaTitulos");
+		mv.addObject("titulos", todosTitulos);
+		return mv;
 	}
 	
 	
--- src/main/java/br/com/gft/model/Titulo.java
+++ src/main/java/br/com/gft/model/Titulo.java
@@ -11,9 +11,16 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.NumberFormat;
+
+
 
 @Entity
 public class Titulo {
@@ -22,17 +29,25 @@ public class Titulo {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
 	
+	@NotEmpty(message = "Descrição obrigatória")
+	@Size(max = 60, message = "Maximo de 60 caracteres")
 	private String descricao;
 	
+	@NotNull(message = "Data obrigatória")
 	@DateTimeFormat(pattern = "dd/MM/yyyy")
 	@Temporal(TemporalType.DATE)
 	private Date dataVencimento;
 	
+	@NotNull(message = "Valor obrigatório")
+	@DecimalMin(value = "0.01", message = "Valor mínimo 0,01")
+	@DecimalMax(value = "9999999.99", message = "Valor máximo 9.999.999,99")
 	@NumberFormat(pattern = "#,##0.00")
 	private BigDecimal valor;
 	
 	@Enumerated(EnumType.STRING)
 	private StatusTitulo status;
+	
+	
 	
 	
 	public Long getCodigo() {
@@ -64,6 +79,10 @@ public class Titulo {
 	}
 	public void setStatus(StatusTitulo status) {
 		this.status = status;
+	}
+	
+	public boolean isPendente() {
+		return StatusTitulo.PENDENTE.equals(this.status);
 	}
 	
 	
